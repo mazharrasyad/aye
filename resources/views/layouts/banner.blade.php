1f77c3b14c475de95f31b8dc754f6513ad853801
mazharrasyad <muhazharrasyad@gmail.com>
2019-12-14 07:14:16 +0700

    credit

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="card bg-dark text-white">
 <img class="card-img" src="{{ asset('assets/img/investasi.jpeg') }}" alt="Card image">
 <div class="card-img-overlay">
-    <h2 class="card-title">Ini tagline...</h2>
+    <h3 class="card-title">"Make Investment, Future Installment"<h3>
 </div>
 </div>
